df9dc7ff06bce1015db9ff88d4f764699fcf72a9
ponmanivasahan <vasupks01@gmail.com>
2026-03-09 18:18:22 +0530

    folders created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import {BrowserRouter} from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 const App = () => {
   return (
-    <div>
-      Hello World  
-    </div>
+    <BrowserRouter>
+       <AppRoutes />
+   </BrowserRouter>
   )
 }
 
---
+++ client/src/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import {Routes,Route} from 'react-router-dom';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='*' element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
