3945464417f8f76c1352a53429a1ebf5c7468390
ponmanivasahan <vasupks01@gmail.com>
2026-03-10 16:09:23 +0530

    merged code

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import {useState,useCallback} from 'react';
+import gsap from "gsap";
 import Loader from '../components/Loader.jsx';
 import Navbar from '../components/Navbar.jsx';
+import Hero from '../sections/Hero.jsx';
+import About from '../sections/About.jsx';
+import Experience from '../sections/Experience.jsx';
+import Projects from '../sections/Projects.jsx';
 const Index = () => {
     const [loading,setLoading] = useState(true);
     const handleLoaderComplete=useCallback(()=> setLoading(false),[]);
     if(loading) return <Loader onComplete={handleLoaderComplete} />;
   return (
-    <div>
+    <>
         <Navbar />
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
       <h1>Welcome to the portfolio</h1>
-    </div>
+    </>
   )
 }
 
