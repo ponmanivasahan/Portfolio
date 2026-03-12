6fa2bb9d5a59c7791ab5186d1a06e5f407a4eb30
ponmanivasahan <vasupks01@gmail.com>
2026-03-12 20:58:45 +0530

    deploy setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import AppRoutes from './AppRoutes.jsx';
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
        <AppRoutes />
-   </BrowserRouter>
+   </HashRouter>
   )
 }
 
